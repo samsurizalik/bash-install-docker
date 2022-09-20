576bad91715b05e790ffdb20b659852ecd8f5079
samsurizalik <111044039+samsurizalik@users.noreply.github.com>
2022-09-20 09:36:43 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,7 +2,7 @@
 echo "Update repo ubuntu"
 sudo apt update
 echo "Install http transport"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 echo "Download gpg repo docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "Add repository docker"
@@ -10,7 +10,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 echo "Check docker list available"
 apt-cache policy docker-ce
 echo "Install Docker & Docker Compose"
-sudo apt install docker-ce docker-compose
+sudo apt install docker-ce docker-compose -y
 echo "Enable start service on StartUP"
 sudo systemctl enable docker
 sudo systemctl status docker
