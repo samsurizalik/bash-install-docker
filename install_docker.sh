d5bb0580c4db9d2a8a8da48345e61a73a84579a6
samsurizalik <111044039+samsurizalik@users.noreply.github.com>
2022-09-20 09:35:05 +0700

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Update repo ubuntu"
+sudo apt update
+echo "Install http transport"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+echo "Download gpg repo docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Add repository docker"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo "Check docker list available"
+apt-cache policy docker-ce
+echo "Install Docker & Docker Compose"
+sudo apt install docker-ce docker-compose
+echo "Enable start service on StartUP"
+sudo systemctl enable docker
+sudo systemctl status docker
